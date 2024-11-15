6afc713a9a4c470d64d2530b2e071df7de0ac366
hionwi <274205761@qq.com>
2024-11-15 13:34:06 +0800

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-export default function Home() {
+import { currentUser } from '@clerk/nextjs/server'
+export default async function Home() {
+  const user = await currentUser()
+
+  const current_user_email = user?.emailAddresses[0].emailAddress
+
+  if (current_user_email !== "jaxprincess127@gmail.com") {
+    return (
+      <div>
+        <main>
+          <h1>
+            Not Authorized
+          </h1>
+        </main>
+      </div>
+    );
+  }
   return (
     <div>
       <main>
         <h1>
-          MAIN
+          Main
         </h1>
       </main>
     </div>
