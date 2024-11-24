d1d843d578d3a227a0cd311c3208bc12fa52fca9
hionwi <274205761@qq.com>
2024-11-24 16:27:54 +0800

    change

--- server/actions/add-data.ts
+++ server/actions/add-data.ts
@@ -1,10 +1,12 @@
 "use server";
 import { db } from "@/server";
 import { postsTable } from "../schema";
+import { revalidatePath } from "next/cache";
 
 export default async function addData(title: string, content: string) {
   await db.insert(postsTable).values({
     title: title,
     content: content,
   });
+  revalidatePath("/");
 }
