ddc9ea72aca57e66b89ff5aa5b8dc51f70ef5d10
hionwi <274205761@qq.com>
2024-11-24 17:17:12 +0800

    change

--- server/actions/get-data.ts
+++ server/actions/get-data.ts
@@ -1,7 +1,10 @@
 "use server";
 import { db } from "@/server";
+import { revalidatePath } from "next/cache";
 
 export default async function getData() {
   const posts = (await db.query.postsTable.findMany()).reverse();
+  revalidatePath("/");
+
   return posts;
 }
