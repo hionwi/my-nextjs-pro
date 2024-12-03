928718593cccf5bd631abbc37e6f45de6ec2ce99
hionwi <274205761@qq.com>
2024-12-03 09:00:00 +0800

    change

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ export default async function Home() {
 
   const sortedData = data.sort((a, b) => b.id - a.id);
   return sortedData.map((item) => (
-    <div key={item.id}>
-      <Article id={item.id} titler={item.title} contentr={item.content} />
-    </div>
+    <Article id={item.id} titler={item.title} contentr={item.content} />
   ));
 }
